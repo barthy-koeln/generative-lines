443f62659a86d76035982e532b18ab86fb747d90
barthy-koeln <post@barthy.koeln>
2026-03-02 14:31:47 +0100

    feat: download

--- src/lib/utils/file.ts
+++ src/lib/utils/file.ts
@@ -1,11 +1,14 @@
-import type { Config } from '../config.ts'
+import type { LinesCanvas } from '../lines-canvas.ts'
 
-export function download(config: Config, canvas: HTMLCanvasElement) {
-  const link = document.createElement("a");
+export function download (linesCanvas: LinesCanvas) {
+  const link = document.createElement('a')
 
-  const valueString = Object.entries(config).map(([name, value]) => `${value}_${name}`).join('-')
+  const valueString = Object
+    .entries(linesCanvas.renderer.config)
+    .map(([name, value]) => `${value}_${name}`)
+    .join('-')
 
-  link.download = `generative_lines-${valueString}.png`;
-  link.href = canvas.toDataURL();
-  link.click();
+  link.href = linesCanvas.renderer.capture()
+  link.download = `generative_lines-${valueString}.png`
+  link.click()
 }
